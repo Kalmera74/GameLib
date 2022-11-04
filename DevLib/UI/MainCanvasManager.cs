f35d6632c81770da942443196109c5c8ad5b090b
Kamil Demirtas <kdemirtas@mobiversite.com>
2022-11-04 18:18:27 +0300

    Canvas manager controller getter

--- DevLib/UI/MainCanvasManager.cs
+++ DevLib/UI/MainCanvasManager.cs
@@ -1,12 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Object = UnityEngine.Object;
 namespace Mobiversite.GameLib.DevLib.UI
 {
     public class MainCanvasManager : MonoBehaviour
     {
         public static MainCanvasManager Instance;
+        [SerializeField] private List<Object> UIManagers = new List<Object>();
+
+
+        public Object Get(Type type)
+        {
+            foreach (var manager in UIManagers)
+            {
+                if (manager.GetType().Equals(type))
+                {
+                    return manager;
+                }
+            }
+            return null;
+        }
 
         void Awake()
         {
