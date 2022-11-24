ceba351d4c4d612edd5b43727924ff641029e44b
Kamil Demirtas <kdemirtas@mobiversite.com>
2022-11-24 17:41:06 +0300

    Vibration Manager  Updated

--- DevLib/Settings/VibrationManager.cs
+++ DevLib/Settings/VibrationManager.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Lofelt.NiceVibrations;
 using Mobiversite.GameLib.DevLib.Core;
 using UnityEngine;
 
 namespace Mobiversite.GameLib.DevLib.Settings
 {
+
     public class VibrationManager : MonoBehaviour
     {
+
+        [SerializeField] private HapticSource Source;
         private bool _canVibrate = true;
+
         public static VibrationManager Instance;
         void Awake()
         {
@@ -20,11 +27,32 @@ namespace Mobiversite.GameLib.DevLib.Settings
             {
                 DestroyImmediate(gameObject);
             }
+
         }
         public void SetCanVibrate(bool canVibrate)
         {
             _canVibrate = canVibrate;
             Save();
+        }
+        public void PlayHaptic(HapticPatterns.PresetType pattern)
+        {
+
+            if (!_canVibrate)
+            {
+                return;
+            }
+            Play(pattern);
+        }
+
+
+
+
+        private void Play(HapticPatterns.PresetType pattern)
+        {
+
+            Source.fallbackPreset = pattern;
+            Source.Play();
+
         }
 
         private void Save()
